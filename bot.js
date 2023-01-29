0f51a77c0aea113288fb8f71f861293a59e8020c
Praakhar7 <120710232+Praakhar7@users.noreply.github.com>
2023-01-29 18:27:31 +0530

    first

---
+++ bot.js
@@ -0,0 +1,62 @@
+const axios = require("axios");
+const { Telegraf } = require("telegraf");
+// THIS TOKEN HAS BEEN REVOKED
+const TOKEN = "5667345992:AAE2xYoRrRFJAktMU5PPicbiyGljD1fUCrc";
+const bot = new Telegraf(TOKEN);
+const Url = 'http://api.weatherstack.com/current?access_key=76f9f4f4fdaa987cad20bc2a32212966&query=New York';
+
+const fetchData = async (cityName) => {
+  const res = await axios.get(`${Url + cityName}`);
+  return res;
+};
+
+
+bot.start((ctx) => {
+  ctx.reply("Hello im a bot");
+});
+
+bot.on("text", async (ctx) => {
+  const { message } = ctx;
+  const { data } = await fetchData(message.text);
+  if (data.success === false) {
+    ctx.reply("Enter a valid city name:");
+  } else {
+    const { current, location } = data;
+    const weatherStatus = current.weather_descriptions[0];
+    ctx.reply(
+      `🌆 City:${location.name}\n-\n 🌡 Temperature ${
+        current.temperature
+      }°\n-\n❓ Weather status: ${
+        (weatherStatus.toLowerCase().includes("clear") === true && "☀️") ||
+        (weatherStatus.toLowerCase().includes("sunny") === true && "☀️") ||
+        (weatherStatus.toLowerCase().includes("cloud") === true && "☁️") ||
+        (weatherStatus.toLowerCase().includes("overcast") === true && "☁️") ||
+        (weatherStatus.toLowerCase().includes("rain") === true && "🌧") ||
+        (weatherStatus.toLowerCase().includes("snow") === true && "❄️")
+      } ${current.weather_descriptions[0]}`
+    );
+    setInterval(() => {
+      console.log('HELLO GEEK');
+
+      
+    ctx.reply(
+      `🌆 City:${location.name}\n-\n 🌡 Temperature ${
+        current.temperature
+      }°\n-\n❓ Weather status: ${
+        (weatherStatus.toLowerCase().includes("clear") === true && "☀️") ||
+        (weatherStatus.toLowerCase().includes("sunny") === true && "☀️") ||
+        (weatherStatus.toLowerCase().includes("cloud") === true && "☁️") ||
+        (weatherStatus.toLowerCase().includes("overcast") === true && "☁️") ||
+        (weatherStatus.toLowerCase().includes("rain") === true && "🌧") ||
+        (weatherStatus.toLowerCase().includes("snow") === true && "❄️")
+      } ${current.weather_descriptions[0]}`
+
+    );
+  }, 3600000);
+  }
+});
+
+bot.launch();
+
+
+console.log("bot start") ;
